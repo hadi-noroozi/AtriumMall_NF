f2be7116a353f251844fdfb4f6cec707b30ee437
hadi-noroozi <hadi.noroozi.72@gmail.com>
2023-01-17 20:39:59 +0330

    final edition of single product

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,11 +199,11 @@ $(document).ready(function () {
 				compact = $this.data('compact'),
                 dateFormat = ( !$this.data('format') ) ? 'DHMS' : $this.data('format'),
                 newLabels = ( !$this.data('labels-short') ) ? 
-                                ['Years', 'Months', 'Weeks', 'Days', 'Hours', 'Minutes', 'Seconds'] :
-                                ['Years', 'Months', 'Weeks', 'Days', 'Hours', 'Mins', 'Secs'],
+                                ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه'] :
+                                ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه'],
                 newLabels1 = ( !$this.data('labels-short') ) ? 
-                                ['Year', 'Month', 'Week', 'Day', 'Hour', 'Minute', 'Second'] :
-                                ['Year', 'Month', 'Week', 'Day', 'Hour', 'Min', 'Sec'];
+                                ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه'] :
+                                ['سال', 'ماه', 'هفته', 'روز', 'ساعت', 'دقیقه', 'ثانیه'];
 
             var newDate;
 
